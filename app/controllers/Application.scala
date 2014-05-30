7733a6d67adca32652b33e2eff8e5fbf60554a2f
crosson.david@gmail.com <crosson.david@gmail.com>
2014-05-30 23:33:29 +0200

    small enhancements

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -6,15 +6,26 @@ import play.api.libs.concurrent.Akka
 import play.api.Play.current
 import akka.actor._
 import java.text.SimpleDateFormat
+import akka.pattern.ask
+import akka.util.Timeout
+import concurrent.duration._
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+
 
 class TrackActor extends Actor {
   val logger = Logger("tracker")
   def now=System.currentTimeMillis()
   val sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S Z")
+  var ips=Set.empty[String]
+  var last:Option[String]=None
   def receive = {
+    case _:StatsRequest => sender ! Stats(ips.size, last)
     case t:TrackIt =>
+      ips += t.remoteAddress
+      val trackDate = sdf.format(now)
+      last = Some(trackDate)
       val entriesExts = List(
-          "track.date"     -> Seq(sdf.format(now)),
+          "track.date"     -> Seq(trackDate),
           "remote.address" -> Seq(t.remoteAddress)
           )
       val msg = 
@@ -29,12 +40,23 @@ class TrackActor extends Actor {
 
 case class TrackIt(remoteAddress:String, entries:Map[String,Seq[String]])
 
+case class StatsRequest()
+
+case class Stats(ipCount:Int=0, lastTimestamp:Option[String]=None)
+
+
 object Application extends Controller {
 
   val tracker = Akka.system.actorOf(Props[TrackActor], name = "trackActor")
   
-  def index = Action {
-    Ok(views.html.index("Track is ready."))
+  def index = Action.async {
+    implicit val timeout = Timeout(5.seconds)
+    val futStats = tracker ? StatsRequest()
+    futStats.map {
+      case s:Stats => Ok(views.html.index(s))
+      case _ => Ok(views.html.index(Stats()))
+    }
+    
   }
 
   def track = Action { request =>
