902ad2e4179f23c78fc88d2f37379eab178a470f
crosson.david@gmail.com <crosson.david@gmail.com>
2015-02-26 23:28:15 +0100

    it works.

--- app/controllers/TrackToCassandraActor.scala
+++ app/controllers/TrackToCassandraActor.scala
@@ -10,8 +10,17 @@ import com.datastax.driver.core.Row
 import com.websudos.phantom.Implicits._
 
 import java.net.InetAddress
-import scala.concurrent.Await
+import scala.concurrent._
 import scala.concurrent.duration._
+
+import com.websudos.phantom.zookeeper.SimpleCassandraConnector
+
+trait Connector extends SimpleCassandraConnector {
+    val keySpace = "tracking"
+}
+
+object Connector extends Connector
+
 
 case class TrackModel (
   id: UUID,
@@ -38,17 +47,44 @@ sealed class TrackRecord extends CassandraTable[TrackRecord, TrackModel] {
 object TrackRecord extends TrackRecord {
   override val tableName="tracked"
   
-  implicit val session = SomeCassandraClient.session
+  implicit val session = Connector.session
   
-  Await.result(create.future(), 5000.millis)
+  def createTableFuture() = {
+    val f = create.future()
+    f.onFailure{case x => x.printStackTrace}
+    f
+  }
+  Await.result(createTableFuture(), 5000.millis)
+  
+  def add(t:TrackThat) = {
+     val fentries = for {
+       (k,values) <- t.entries.toList
+       } yield {
+       values.toList match {
+         case Nil => Nil
+         case v::Nil => List(k->v)
+         case all => all.zipWithIndex.map{case (v,i) => (k+"."+(i+1)) -> v}
+       }
+     }
+     insert
+        .value(_.id, UUID.randomUUID())
+        .value(_.timestamp, new DateTime(t.when))
+        .value(_.category, t.category)
+        .value(_.inet, InetAddress.getByName(t.remoteAddress))
+        .value(_.entries, fentries.flatten.toMap)
+        .future()
+  }
+  
 }
 
 
 
 class TrackToCassandraActor extends Actor {
+  import scala.concurrent.ExecutionContext.Implicits.global
   
   def receive = {
     case _:StatsRequest => sender ! Stats(0, None)
     case t:TrackThat =>
+      TrackRecord.add(t).onFailure{case x => x.printStackTrace}
   }
 }
--- build.sbt
+++ build.sbt
@@ -1,12 +1,17 @@
 
 name := "track"
 
-scalaVersion := "2.11.1"
+scalaVersion := "2.11.5"
 
 version := "1.1-SNAPSHOT"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
 
 libraryDependencies ++= Seq(
-  "com.websudos"  %% "phantom-dsl"  % "1.5.0"
+  "com.websudos"  %% "phantom-dsl"       % "1.5.0",
+  "com.websudos"  %% "phantom-zookeeper" % "1.5.0"
 )
+
+resolvers += "twitter-repo" at "http://maven.twttr.com"
+
+resolvers += "websudos-repo" at "http://maven.websudos.co.uk/ext-release-local"
